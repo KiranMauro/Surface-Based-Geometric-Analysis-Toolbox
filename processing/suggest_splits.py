f0ac24a691b07ccbe31d3af5806180eedfcc5e89
KiranMauro <126411166+KiranMauro@users.noreply.github.com>
2026-07-28 16:36:38 +0200

    Update suggest_splits.py

--- processing/suggest_splits.py
+++ processing/suggest_splits.py
@@ -72,7 +72,7 @@ def run(project):
         if np.any(np.isnan(v2)):
             return 0.0
 
-        c = np.clip(np.dot(v1, v2)), -1.0, 1.0
+        c = np.clip(np.dot(v1, v2), -1.0, 1.0)
 
         return abs(c)
 
